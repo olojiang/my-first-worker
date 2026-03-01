3b6af30df42fcdf632769afe7cd391b7e831c221
Admin <admin@example.com>
2026-03-02 00:04:46 +0800

    chore: update eruda to latest and move to head section

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 // 自动生成的版本号文件
 // 格式: MMDD_HHmm
-export const BUILD_TIME = '0301_2337'
-export const APP_VERSION = '1.0.8'
+export const BUILD_TIME = '0302_0004'
+export const APP_VERSION = '1.0.9'
 export const VERSION_DISPLAY = `${APP_VERSION}_${BUILD_TIME}`
