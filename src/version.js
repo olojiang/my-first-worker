d293b95082e06114139dffb7ca0f77fac603aee1
Admin <admin@example.com>
2026-03-01 23:10:03 +0800

    chore: update eruda files and bump version

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 // 自动生成的版本号文件
 // 格式: MMDD_HHmm
-export const BUILD_TIME = '0301_2308'
-export const APP_VERSION = '1.0.3'
+export const BUILD_TIME = '0301_2309'
+export const APP_VERSION = '1.0.4'
 export const VERSION_DISPLAY = `${APP_VERSION}_${BUILD_TIME}`
